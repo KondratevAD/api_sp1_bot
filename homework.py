6b8e12344b8f7c5bab88375f3f34414a954f3a8f
KondratevAD <Kondratev_ad@bk.ru>
2021-05-01 21:44:21 +0300

    First check

--- homework.py
+++ homework.py
@@ -18,18 +18,21 @@ logging.basicConfig(
     level=logging.DEBUG,
     format='%(asctime)s, %(levelname)s, %(name)s, %(message)s',
     filename='main.log',
-) 
+)
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 handler = RotatingFileHandler('main.log', maxBytes=50000000, backupCount=5)
-logger.addHandler(handler) 
+logger.addHandler(handler)
+
 
 def parse_homework_status(homework):
     homework_name = homework.get('homework_name')
     if homework.get('status') == 'rejected':
         verdict = 'К сожалению в работе нашлись ошибки.'
     else:
-        verdict = 'Ревьюеру всё понравилось, можно приступать к следующему уроку.'
+        verdict = ('Ревьюеру всё понравилось, '
+            'можно приступать к следующему уроку.'
+        )
     return f'У вас проверили работу "{homework_name}"!\n\n{verdict}'
 
 
@@ -60,9 +63,15 @@ def main():
         try:
             new_homework = get_homework_statuses(current_timestamp)
             if new_homework.get('homeworks'):
-                send_message(parse_homework_status(new_homework.get('homeworks')[0]), bot_client)
+                send_message(parse_homework_status(
+                    new_homework.get('homeworks')[0]),
+                    bot_client
+                )
                 logging.info('Отправка сообщения')
-            current_timestamp = new_homework.get('current_date', current_timestamp)  # обновить timestamp
+            current_timestamp = new_homework.get(
+                'current_date',
+                current_timestamp
+            )
             time.sleep(300)  # опрашивать раз в пять минут
 
         except Exception as e:
